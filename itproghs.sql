05b1606efde9a763b3c531368c5b8da980d0e5e7
yvesjmnz <yves_alvin_jimenez@dlsu.edu.ph>
2024-07-22 18:34:13 +0800

    added bookings and rooms in database

--- itproghs.sql
+++ itproghs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2024 at 06:15 PM
+-- Generation Time: Jul 22, 2024 at 12:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,40 @@ SET time_zone = "+00:00";
 --
 -- Database: `itproghs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `nights` int(11) DEFAULT NULL,
+  `booked_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `price_per_night` decimal(10,2) DEFAULT NULL,
+  `availability` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `type`, `price_per_night`, `availability`) VALUES
+(1, 'Single', 799.00, 1);
 
 -- --------------------------------------------------------
 
@@ -43,7 +77,51 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`firstName`, `lastName`, `email`, `phoneNumber`, `password`, `profilePicture`, `admin_checker`) VALUES
 ('Barry', 'Ran', 'harry@gmail.com', '+639665004905', '$2y$10$wEwOQT3EsEhLpQmOpgJtCuwSBhBmBkSLqtixOjGfLerOEnWAUBr.e', 0x75706c6f6164732f3132332e6a7067, 0),
-('Garry', 'Ray', 'garry@gmail.com', '+639665004905', '$2y$10$0H7gah3WrBX57VIvkZKQn.tmR/gC0zhrZl33iVXE87dAmWcvogmBa', 0x75706c6f6164732f3132332e6a7067, 0);
+('Garry', 'Ray', 'garry@gmail.com', '+639665004905', '$2y$10$0H7gah3WrBX57VIvkZKQn.tmR/gC0zhrZl33iVXE87dAmWcvogmBa', 0x75706c6f6164732f3132332e6a7067, 0),
+('Yves', 'Jimenez', 'yvesandrei02@gmail.com', '09173175660', '$2y$10$MC8zhQE3aSAoYsh2lyKtreUcphPvKBNsL0gxiv1IJRwNI6RLrcsCW', 0x75706c6f6164732f324b734d614b694d6d4d6562453332756b4858585a372e6a7067, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
