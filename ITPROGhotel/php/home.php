5a784771fabc1640e7c1b270eb01a4ef7a22e9f5
lilraffyel <rafael_calalang@dlsu.edu.ph>
2024-07-28 21:36:31 +0800

    Updated Design for Menu/Home
    - Restructured some of the HTML but reused some of the existing buttons
    - Converted the nav bar into the side bar according to the planned design
    - Added 'Profile' button at sidebar
    - Adjusted 'About Us' button to be below logo
    - Pressing logo should redirect user back to home/meu page
    
    NOTE: Commented out old CSS and HTML code just in case

--- ITPROGhotel/php/home.php
+++ ITPROGhotel/php/home.php
@@ -1,3 +1,9 @@
+<!--TODO
+    - Consult with group on design
+    - Minor adjustments with sidebar (if needed)
+    - Adjustments to header (see styles.css)
+-->
+
 <?php
 session_start();
 if (!isset($_SESSION['loggedin'])) {
@@ -13,19 +19,57 @@ if (!isset($_SESSION['loggedin'])) {
     <title>Home Page</title>
     <link href="../styles.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body class="loggedin">
-    <nav class="navtop">
+    <div class="header">
         <div>
-            <h1>MotelEase</h1>
-            <a href="home.php"></i>Home</a>
+            <h1>MENU</h1>
+            <!-- <a href="home.php"></i>Home</a>
+            <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a> -->
+        </div>
+    </div>
+
+    <nav class = "sidebar">
+        <a href = "home.php"> <img src = "" alt = "MotelEase Logo"> </a>
+        <a href="aboutus.php" class="about-us">About us</a>
+        <div class = "sidebar-bottom">
             <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
             <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
         </div>
     </nav>
+
     <div class="content">
-        <h2>Welcome to MotelEase</h2>
-        <p>Container for content <33</p>
+        <!-- <h2>Welcome to MotelEase</h2>
+        <p>Container for content <33</p> -->
+        <div class = "grid-container">
+                <div class = "grid-element">
+                    <h3> Room Selection </h3>
+                    <p> Book and check the list of available toom types </p>
+                </div>
+                <div class = "grid-element">
+                    <h3> Summary </h3>
+                    <p> Booking Summary <br>
+                        - Apply promotions <br>
+                        - Summary of selected room/s, amenities and total price 
+                    </p>
+                </div>
+                <div class = "grid-element">
+                    <h3> Amenities </h3>
+                    <p> List of available amenities </p>
+                </div>
+                <div class = "grid-element">
+                    <h3> Checkout </h3>
+                    <p> - Payment Options <br>
+                        - Confirm/Cancel your booking
+                    </p>
+                </div>
+        </div>
     </div>
 </body>
 </html>
