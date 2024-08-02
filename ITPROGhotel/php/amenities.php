e81e5bd279320ebaeaa493b28838e957a46c1120
lilraffyel <rafael_calalang@dlsu.edu.ph>
2024-08-02 16:03:55 +0800

    Updated Amenities
    - Similar to Rooms but different colors

--- ITPROGhotel/php/amenities.php
+++ ITPROGhotel/php/amenities.php
@@ -24,40 +24,61 @@ $conn->close();
 <head>
     <meta charset="utf-8">
     <title>Amenities</title>
-    <link href="../styles.css" rel="stylesheet" type="text/css">
+    <link href="amenities.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body class="loggedin">
-    <div class="header">
-        <h1>Amenities</h1>
-    </div>
-    <nav class="sidebar">
-        <a href="home.php"><img src="" alt="MotelEase Logo"></a>
-        <a href="aboutus.html" class="about-us">About us</a>
-        <div class="sidebar-bottom">
-            <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-            <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+    <nav class="navtop">
+        <div class="nav-container">
+            <div class="logo">
+                <a href="home.php"><img src="" alt="MotelEase Logo"></a>
+            </div>
+            <div class="nav-links">
+                <a href="aboutus.html" class="nav-link">About us</a>
+                <a href="profile.php" class="nav-link"><i class="fas fa-user-circle"></i> Profile</a>
+                <a href="logout.php" class="nav-link"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
         </div>
     </nav>
+
     <div class="content">
+        <p><a href="home.php" class="back-btn"> ← Return to Main Menu</a></p>
         <h2>Available Amenities</h2>
-        <div class="grid-container">
-            <?php foreach ($amenities as $amenity): ?>
-                <div class="grid-element">
-                    <p>Amenity: <?= htmlspecialchars($amenity['name']) ?></p>
-                    <p>Description: <?= htmlspecialchars($amenity['description']) ?></p>
-                    <p>Price: <?= htmlspecialchars($amenity['price']) ?> PHP</p>
-                    <form action="add-to-cart.php" method="post">
-                        <input type="hidden" name="type" value="amenity">
-                        <input type="hidden" name="id" value="<?= htmlspecialchars($amenity['id']) ?>">
-                        <input type="hidden" name="name" value="<?= htmlspecialchars($amenity['name']) ?>">
-                        <input type="hidden" name="price" value="<?= htmlspecialchars($amenity['price']) ?>">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" name="quantity" min="1" max="10" required>
-                        <input type="submit" value="Add to Cart" class="grid-item button-item">
-                    </form>
-                </div>
-            <?php endforeach; ?>
-        </div>
+        <table class="rooms-table">
+            <thead>
+                <tr>
+                    <th>Amenity</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Number of Days/Pax</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($amenities as $amenity): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($amenity['name']) ?></td>
+                        <td><?= htmlspecialchars($amenity['description']) ?></td>
+                        <td><?= htmlspecialchars($amenity['price']) ?> PHP</td>
+                        <td>
+                            <form action="add-to-cart.php" method="post">
+                                <input type="hidden" name="type" value="amenity">
+                                <input type="hidden" name="id" value="<?= htmlspecialchars($amenity['id']) ?>">
+                                <input type="hidden" name="name" value="<?= htmlspecialchars($amenity['name']) ?>">
+                                <input type="hidden" name="price" value="<?= htmlspecialchars($amenity['price']) ?>">
+                                <input type="number" name="quantity" min="1" max="10" required>                        </td>
+                        <td>
+                            <input type="submit" value="Add to Cart" class="button-item">
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
